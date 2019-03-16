29d197f7e4a5112c22e49ed8dfa7d6dd65a94a20
Vikram Aroskar <vikram.aroskar@gmail.com>
2019-03-16 11:19:58 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-import {getHTML, getTwitterFollowers, getInstagramFollowers} from './lib/scraper.js';
+import express from 'express';
+import {getHTML, getTwitterCount, getInstagramCount} from './lib/scraper.js';
+import db from './lib/db';
+import './lib/cron.js';
 
+
+
+//create new express app
+const app = new express();
+
+console.log(db);
+
+
+app.get('/scrape', async (request, response, next) =>  {
+  console.log('scraping !!');
+  const twitterCount = await getTwitterCount();
+  const instaCount = await getInstagramCount();
+  console.log(twitterCount, instaCount);
+  /* the scrape url will now not save to the db, it will be just be to give live count in GET call
+  db
+    .get('twitter').push({
+      date: Date.now(),
+      count: twitterCount
+    })
+    .write();
+  db
+    .get('instagram').push({
+      date: Date.now(),
+      count: instaCount
+    })
+    .write();
+  */
+  response.json({twitterCount, instaCount});
+  
+
+});
+
+
+app.listen(3333, () => {
+  console.log('Example App Started');
+  console.log('Example App Running for webscraping on port 3333');
+});
+
+
+/*
 async function go() {
   // console.log(await getHTML('https://twitter.com/vikramaroskar'));
   //const html = await getHTML('https://twitter.com/vikramaroskar');
@@ -10,18 +53,24 @@ async function go() {
   //const htmlinsta = await getHTML('https://www.instagram.com/wesbos/');
   //const instaFollowers = await getInstagramFollowers(htmlinsta);
   //console.log(instaFollowers);
-  const tPromise = getHTML('https://twitter.com/vikramaroskar');
-  const iPromise = getHTML('https://www.instagram.com/wesbos/');
 
-  const [iHtml, tHtml] = await Promise.all([iPromise, tPromise]);
-  const instaCount = await getInstagramFollowers(iHtml);
-  const twCount = await getTwitterFollowers(tHtml);
-  console.log( `You have ${twCount} Twitter followers`);
-  console.log( `Wesbos has ${instaCount} Instagram followers`);
+  //SECOND METHOD 
+  //const tPromise = getHTML('https://twitter.com/vikramaroskar'); 
+  //const iPromise = getHTML('https://www.instagram.com/wesbos/');
+  //const [iHtml, tHtml] = await Promise.all([iPromise, tPromise]);
+  //const instaCount = await getInstagramFollowers(iHtml);
+  //const twCount = await getTwitterFollowers(tHtml);
+  //console.log( `You have ${twCount} Twitter followers`);
+  //console.log( `Wesbos has ${instaCount} Instagram followers`);
+
+  //THIRD METHOD
+  //const twitterCount = await getTwitterCount();
+  //const instaCount = await getInstagramCount();
+  //console.log(twitterCount, instaCount);
   
   
 
 }
 
 go();
-
+*/
