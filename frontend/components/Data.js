02afa36d80d4ddfe360645a519e25ac0eafe38a9
Vikram Aroskar <vikram.aroskar@gmail.com>
2019-03-20 17:17:07 +0530

    Add files via upload

--- frontend/components/Data.js
+++ frontend/components/Data.js
@@ -1,23 +1,20 @@
 import  {useContext} from 'react';
 import  {ScrapeContext} from './ScrapeContext';
-import  {distanceInWords}  from 'date-fns';
+import Table from './Table';
+import Chart from './Chart';
 
 export default function Data() {
   const {scrapes} = useContext(ScrapeContext);
   console.log(scrapes);
   return (
   <div>
-   <h2> Your Data: </h2>
-   <ul>
-    {scrapes.twitter.map(scrape => 
-      <li key={scrape.date}>
-      {scrape.count} 
-      - 
-      {distanceInWords(new Date(scrape.date),
-                        new Date() 
-      )}</li>)}
-   </ul>
-   {scrapes.twitter.length}
+   <h2> Twitter: </h2>
+   <Table scrapes= {scrapes.twitter} />
+   <Chart scrapes= {scrapes.twitter} />
+
+   <h2> Instagram: </h2>
+   <Table scrapes= {scrapes.instagram} />
+   <Chart scrapes= {scrapes.instagram} />
   </div>
   );
 }
---
+++ frontend/components/Table.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import  {distanceInWords}  from 'date-fns';
+
+
+export default function Table({scrapes}) {
+  const scrapesReversed = [...scrapes].reverse();
+  return (
+     <table>
+       <thead>
+         <tr>
+          <td>Count</td>
+          <td>Time</td>
+         </tr>
+       </thead>
+       <tbody>
+          {scrapesReversed.map(scrape => 
+            <tr key={scrape.date}>
+              <td>{scrape.count} </td>
+              <td>{distanceInWords(new Date(scrape.date),
+                                new Date() 
+                )}</td>
+            </tr>)}
+       </tbody>
+     </table>
+   );
+}
